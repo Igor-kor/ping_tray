a4c5af85b3812d70a3cd4814105564ecd6e1f10a
igor <igor_korenovsk@mail.ru>
2017-05-10 00:55:17 +0300

    initial commit

---
+++ ping_tray/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ping_tray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ping xxx.xxx.xxx.xxx";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.1.1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ping ip ->";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "intarval (ms)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(227, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "100";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(336, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ok";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(443, 64);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Ping Tray";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.MinimumSizeChanged += new System.EventHandler(this.Form1_MinimumSizeChanged);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ping_tray/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ping_tray
+{
+    public partial class Form1 : Form
+    {
+        String IpAddr = "127.0.0.1";
+        System.Drawing.Icon iconGreen, iconRed;
+        public Form1()
+        {
+            InitializeComponent();
+            iconGreen = new Icon("policegreen.ico");
+            iconRed = new Icon("policereed.ico");
+            notifyIcon1.Icon = iconGreen;
+            IpAddr = textBox1.Text;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply;
+            try
+            {
+                reply = pingSender.Send(IpAddr, timeout, buffer, options);
+            }
+             catch (System.Net.NetworkInformation.PingException)
+            {
+                notifyIcon1.Icon = iconRed;
+                return;
+            }
+            catch (System.ArgumentNullException)
+            {
+                notifyIcon1.Icon = iconRed;
+                return;
+            }
+
+            notifyIcon1.Text = "Ping: "+ textBox1.Text;
+            if (reply.Status == IPStatus.Success)
+            {
+                notifyIcon1.Icon = iconGreen;
+            }
+            if(reply.Status == IPStatus.TimedOut || reply.Status == IPStatus.BadDestination
+                || reply.Status == IPStatus.BadHeader
+                || reply.Status == IPStatus.BadOption
+                || reply.Status == IPStatus.BadRoute
+                || reply.Status == IPStatus.DestinationHostUnreachable
+                || reply.Status == IPStatus.DestinationNetworkUnreachable
+                || reply.Status == IPStatus.DestinationPortUnreachable
+                || reply.Status == IPStatus.DestinationProhibited
+                || reply.Status == IPStatus.DestinationProtocolUnreachable
+                || reply.Status == IPStatus.DestinationScopeMismatch
+                || reply.Status == IPStatus.DestinationUnreachable
+                || reply.Status == IPStatus.HardwareError
+                || reply.Status == IPStatus.IcmpError
+                || reply.Status == IPStatus.NoResources
+                || reply.Status == IPStatus.PacketTooBig
+                || reply.Status == IPStatus.ParameterProblem
+                || reply.Status == IPStatus.SourceQuench
+                || reply.Status == IPStatus.TimeExceeded
+                || reply.Status == IPStatus.TtlExpired
+                || reply.Status == IPStatus.TtlReassemblyTimeExceeded
+                || reply.Status == IPStatus.Unknown
+                || reply.Status == IPStatus.UnrecognizedNextHeader)
+            {
+                notifyIcon1.Icon = iconRed;       
+            }
+
+        }
+
+        private void Form1_MinimumSizeChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IpAddr = textBox1.Text;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // notifyIcon1.Visible = true;
+            WindowState = FormWindowState.Normal;
+            Hide();          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(textBox2.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            WindowState = FormWindowState.Normal;
+            Show();
+        }
+    }
+}
